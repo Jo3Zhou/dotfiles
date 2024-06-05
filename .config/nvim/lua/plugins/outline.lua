5d35107462c331162dcf40e69268d2a5696cd1ca
Jialu Zhou <jialu.zhou@viperinnovations.com>
2024-06-05 21:26:51 +0100

    update outline config

--- .config/nvim/lua/plugins/outline.lua
+++ .config/nvim/lua/plugins/outline.lua
@@ -1,12 +1,33 @@
 return {
 	"hedyhli/outline.nvim",
 	lazy = true,
-	config = function()
-		-- Example mapping to toggle outline
-		vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+	cmd = { "Outline", "OutlineOpen" },
+	keys = { -- Example mapping to toggle outline
+		{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+	},
+	opts = function()
+		local Config = require("lazyvim.config")
+		local defaults = require("outline.config").defaults
+		local opts = {
+			symbols = {},
+			symbol_blacklist = {},
+		}
+		local filter = Config.kind_filter
 
-		require("outline").setup({
-			-- Your setup opts here (leave empty to use defaults)
-		})
+		if type(filter) == "table" then
+			filter = filter.default
+			if type(filter) == "table" then
+				for kind, symbol in pairs(defaults.symbols) do
+					opts.symbols[kind] = {
+						icon = Config.icons.kinds[kind] or symbol.icon,
+						hl = symbol.hl,
+					}
+					if not vim.tbl_contains(filter, kind) then
+						table.insert(opts.symbol_blacklist, kind)
+					end
+				end
+			end
+		end
+		return opts
 	end,
 }
