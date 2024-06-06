124aed8dbd604ae187466e6008b1a221181e5f72
Jialu Zhou <jialu.zhou@viperinnovations.com>
2024-06-06 11:27:23 +0100

    add enable spell check by default & change spelling to 'en_gb'

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+-- Enable spell checker
+vim.opt.spelllang = "en_gb"
+vim.opt.spell = true
+
+-- Neovide options
 if vim.g.neovide then
 	-- Put anything you want to happen only in Neovide here
 	vim.g.neovide_cursor_vfx_mode = "pixiedust"
