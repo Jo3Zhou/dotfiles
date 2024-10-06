697facc69d0f431134d8848d889e25958c916908
Jialu Zhou <jialu.zhou@viperinnovations.com>
2024-10-06 19:33:57 +0100

    disable italic for rose-pine.lua

--- .config/nvim/lua/plugins/rosepine.lua
+++ .config/nvim/lua/plugins/rosepine.lua
@@ -1,1 +1,24 @@
-return { "rose-pine/neovim", priority = 10000, name = "rose-pine" }
+return {
+	"rose-pine/neovim",
+
+	config = function()
+		require("rose-pine").setup({
+			variant = "auto", -- auto, main, moon, or dawn
+			dark_variant = "main", -- main, moon, or dawn
+			dim_inactive_windows = false,
+			extend_background_behind_borders = true,
+
+			enable = {
+				terminal = true,
+				legacy_highlights = true,
+				migrations = true,
+			},
+
+			styles = {
+				bold = true,
+				italic = false,
+				transparency = false,
+			},
+		})
+	end,
+}
