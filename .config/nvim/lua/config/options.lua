4809ab2fb0f20aa8b65f8dd450b1abaa94a3f684
Jialu Zhou <jialu.zhou@viperinnovations.com>
2026-08-24 10:57:30 +0100

    add osc 52 support for neovim and kitty

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,3 +10,17 @@ vim.opt.textwidth = 80
 
 -- Disable animations
 vim.g.snacks_animate = false
+
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
+
+vim.opt.clipboard = "unnamedplus"
