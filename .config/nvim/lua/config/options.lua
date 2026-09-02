7cf2984acdd01ca6b591684d7a1594d1ecf16efc
Jialu Zhou <jialu.zhou@viperinnovations.com>
2026-09-02 15:58:21 +0100

    fix local clipboard for neovim, fix jetbrain nerd font, add large file support for .gitconfig

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,26 +1,25 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
--- Enable spell checker
 vim.opt.spelllang = "en_gb"
 vim.opt.spell = true
-
--- Text width
 vim.opt.textwidth = 80
-
--- Disable animations
 vim.g.snacks_animate = false
 
-vim.g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	},
-	paste = {
-		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-	},
-}
+if vim.env.SSH_TTY or vim.env.SSH_CONNECTION then
+	local osc52 = require("vim.ui.clipboard.osc52")
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = { ["+"] = osc52.copy("+"), ["*"] = osc52.copy("*") },
+		paste = { ["+"] = osc52.paste("+"), ["*"] = osc52.paste("*") },
+	}
+else
+	vim.g.clipboard = {
+		name = "wl-clipboard",
+		copy = { ["+"] = "wl-copy", ["*"] = "wl-copy --primary" },
+		paste = { ["+"] = "wl-paste", ["*"] = "wl-paste --primary" },
+		cache_enabled = 0,
+	}
+end
 
 vim.opt.clipboard = "unnamedplus"
